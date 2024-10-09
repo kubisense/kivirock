00cf0d3c03adad663307d417548bc23dab311120
Clever Ubiagege <clever.ubiagege@yahoo.com>
2024-10-09 09:25:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to Kivirock Group Limited!");
+});
+/*
+Kivirock is a real estate investment platform It is a subsidiary of the kivirock Group Limited, that is focused mainly on infracturural investments and managing of underlying assets
+*/
