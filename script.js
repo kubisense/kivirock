f371a30c0ecb3905403b690782914e1159559d1d
Clever Ubiagege <clever.ubiagege@yahoo.com>
2024-10-14 22:02:37 +0100

    updated header section of index.html and styles.css to allow mobile friendliness

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Welcome to Kivirock Group Limited!");
 });
+
+// Function to toggle the visibility of the collapsible menu
+function toggleMenu() {
+  const menu = document.getElementById("collapsibleMenu");
+  const btnStatus = document.getElementById("btnVisibility");
+  const btnClose = document.getElementById("btnClose");
+  // Toggle between showing and hiding the close button
+  // Toggle between showing and hiding the menu
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    btnClose.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    btnStatus.style.display = "none";
+    btnClose.style.display = "block";
+  }
+}
 /*
 Kivirock is a real estate investment platform It is a subsidiary of the kivirock Group Limited, that is focused mainly on infracturural investments and managing of underlying assets
 */
